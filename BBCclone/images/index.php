63cf016edb5ae3c0c9e7b446178f98c0305034be
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2017-11-16 01:41:44 -0800

    added images to bbc clone

---
+++ BBCclone/images/index.php
@@ -0,0 +1,89 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Hello World!</title>
+    
+    <style type="text/css">
+   
+    </style>
+    
+  </head>
+  <body>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/DrDm7uO4Fu0" frameborder="0" allowfullscreen align="right"></iframe>
+
+          <p><h1>Hello World!</h1></p>
+          <p><h2>A <strong>few </strong>few facts about the world we live in</h2></p>
+          <img src="earth.jpg" height="250" width="250" />
+        <p>Earth is the third planet from the sun and is the largest of the terrestrial planets.
+        Unlike the other planets in the solar system that are named after classic deities, the
+        Earth's name comes from the Anglo-Saxon word erda which means ground or soil. The Earth
+        was formed approximately 4.54 billion years ago and is the only known planet to support life.
+        </p>
+        <hr>
+        <h2>About the earth</h2>
+            <ul>
+                <li><strong>Mass:</strong> 5,972,190,000,000,000 billion kg</li>
+                <li><strong>Diameter at equator:</strong> 12,756 km</li>
+                <li><strong>Satellites:</strong> <a href="http://space-facts.com/the-moon/">The Moon</a></li>
+            </ul>
+        
+        <hr>
+        
+        <h2>Top 3 earth facts</h2>
+        <ol>
+        <li>The Earth's rotation is gradually slowing.</li>
+        <li>A year isn't exactly 365 days, and earth doesn't take 24 hours to rotate</li>
+        <li>70% of the earth's surface is water, and only one third of its fresh water is unfrozen</li>
+        </ol>
+        
+        <hr>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Radius</th>
+                    <th>Average <br>Temperature</th>
+                </tr>
+        </thead>
+        <tbody>
+               <tr>
+                   <td>Mars</td>
+                     <td>3,390km</td>
+                   <td>-55&deg;C</td>
+              </tr>
+             <tr>
+                  <td>Venus</td>
+                 <td>6,052km</td>
+                 <td>460&deg;C</td>
+            </tr>
+        </tbody>
+        </table>
+        
+        <hr>
+        
+        <p>Want to know more?</p>
+        
+        <form action="http://space-facts.com/earth/">
+              <input type="submit" value="Click Here!"></input>
+        </form>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
